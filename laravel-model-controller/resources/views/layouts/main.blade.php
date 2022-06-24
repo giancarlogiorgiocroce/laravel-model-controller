064cb29ac55d9bc2d256be384d3ece0bc8fcb631
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-06-24 17:47:05 +0200

    Aggiunto un po' di css (Io e l'arte siamo ai due opposti dello spettro)

--- laravel-model-controller/resources/views/layouts/main.blade.php
+++ laravel-model-controller/resources/views/layouts/main.blade.php
@@ -4,20 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lar Model Contr | @yield('title')</title>
+
+    <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
+
+    <title>Laravel Mod & Ctrl | @yield('title')</title>
 </head>
 <body>
 
-    <nav>
-        <ul>
-            <li><a href="{{ route('home') }}">Home</a></li>
-            <li><a href="{{ route('about') }}">About</a></li>
-            <li><a href="{{ route('contacts') }}">Contacts</a></li>
-            <li><a href="{{ route('movies') }}">Movies</a></li>
-        </ul>
-    </nav>
+    <div class="container">
+        <nav>
+            <ul>
+                <li><a href="{{ route('home') }}">Home</a></li>
+                <li><a href="{{ route('about') }}">About</a></li>
+                <li><a href="{{ route('contacts') }}">Contacts</a></li>
+                <li><a href="{{ route('movies') }}">Movies</a></li>
+            </ul>
+        </nav>
 
-    @yield('content')
+        @yield('content')
+    </div>
 
 </body>
 </html>
